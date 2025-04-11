a31ed94e6c80a3dc3ce92ecec831ac9991d3fb61
G.M <g.mjardev@gmail.com>
2025-04-11 17:00:36 -0500

    1. use concept to limit precision.

--- FixedPointPrecision/fixed.cxx
+++ FixedPointPrecision/fixed.cxx
@@ -1,20 +1,22 @@
 module;
 #include<concepts>
 #include<cstdint>
+#include<climits>
+#include<type_traits>
 export module fixed;
+using namespace std;
 export{
-    template<std::signed_integral Bone,uint8_t at>
+    template<signed_integral Bone,uint8_t Precision> requires(sizeof(Bone)*CHAR_BIT>=Precision)
     class fx
     {
         public:
         fx(const Bone& v):v(v){}
         fx(const fx&o):v(o.v){}
-        fx operator+(const fx& o)const
+        fx operator+(const fx& o)const noexcept
         {
             return fx(v+o.v);
         }
-Bone v;
     private:
-
+Bone v;
     };
 }
--- FixedPointPrecision/main.cpp
+++ FixedPointPrecision/main.cpp
@@ -3,7 +3,7 @@
 import fixed;
 int main()
 {
-    fx<int8_t,4> a=0;
-    fx<int8_t,4> b=1;
+    fx<int8_t,8> a=0;
+    fx<int8_t,8> b=1;
     std::cout<<+(a+b).v;
 }
