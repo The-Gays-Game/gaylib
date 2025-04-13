a7d0aebd71902c07a57c0cb3243c49edd3dd6101
g.m <g.mjardev@gmail.com>
2025-04-12 19:55:26 -0500

    1. float cast working.

--- FixedPointPrecision/fixed.cxx
+++ FixedPointPrecision/fixed.cxx
@@ -2,9 +2,47 @@ module;
 #include<concepts>
 #include<cstdint>
 #include<climits>
+#include<bit>
+#include<tuple>
 #include<type_traits>
+#include<iostream>
 export module fixed;
 using namespace std;
+
+//template<class T> concept IEEE754Size=;
+
+template<class T>requires same_as<T,uint64_t>||same_as<T,uint32_t>
+struct IEEE754 {
+    using underlying=T;
+    uint8_t exponentBits,fractionBits;
+    uint16_t exponentBias;
+    uint16_t constexpr truncateExponent(uint16_t exponent)const noexcept {
+        return exponent&(UINT16_MAX>>(16-exponentBits));
+    }
+};
+constexpr IEEE754<uint32_t> F32{8,23,127};
+constexpr IEEE754<uint64_t>F64{11,52,1023};;
+
+template<auto const fmt,unsigned_integral B,class U=typename decltype(fmt)::underlying>constexpr
+U toFloatFmtU(B v,uint8_t precision)noexcept {
+    constexpr uint8_t boneSize=sizeof(B)*CHAR_BIT;
+    uint8_t leading0=countl_zero(v);
+    v<<=leading0;//drop first bit
+    v<<=1;//when leading0+1==boneSize, undefined.
+    U fraction;
+    if constexpr(boneSize>=fmt.fractionBits) //keep only first fractionBits bits.
+        fraction=v>>(boneSize-fmt.fractionBits);
+    else //lshift leading bit to fractionBit
+        fraction=U(v)<<(fmt.fractionBits-boneSize);
+    uint8_t exponent=boneSize-leading0-precision-1;
+    return fmt.truncateExponent(exponent+fmt.exponentBias)<<fmt.fractionBits|fraction;
+}
+template<auto const fmt,signed_integral B,class U = typename decltype(fmt)::underlying>constexpr
+U toFloatFmt(B v,uint8_t precision)noexcept {
+    uint8_t sign=v<0;
+    U r=toFloatFmtU<fmt,make_unsigned_t<B>>(abs(v),precision);
+    return sign << fmt.exponentBits + fmt.fractionBits |r;
+}
 export{
     template<signed_integral Bone,uint8_t Precision> requires(sizeof(Bone)*CHAR_BIT>=Precision)
     class fx
@@ -15,6 +53,12 @@ export{
         fx operator+(const fx& o)const noexcept
         {
             return fx(v+o.v);
+        }
+        constexpr explicit operator float ()const {
+            return bit_cast<float>(toFloatFmt<F32>(v,Precision));
+        }
+        constexpr explicit operator double ()const {
+            return bit_cast<double>(toFloatFmt<F64>(v,Precision));
         }
     private:
 Bone v;
--- FixedPointPrecision/main.cpp
+++ FixedPointPrecision/main.cpp
@@ -3,7 +3,15 @@
 import fixed;
 int main()
 {
-    fx<int8_t,8> a=0;
-    fx<int8_t,8> b=1;
-    std::cout<<+(a+b).v;
+    fx<int32_t,3> a=0b1000;
+    std::cout<< static_cast<float>(a)<<std::endl;
+    fx<int32_t,3>b=0b1100011;
+    std::cout<< static_cast<float>(b)<<std::endl;
+    fx<int32_t,2>c=0b1;
+    std::cout<< static_cast<float>(c)<<std::endl;
+    fx<int32_t,0>d=0b100000000000000000000000;
+    std::cout<< static_cast<float>(d)<<std::endl;
+    uint8_t f=1;
+    uint32_t g=0;
+    std::cout<<(unsigned int)(f<<12)<<std::endl;
 }
